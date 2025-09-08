c40d00337249f3750874aee1638c00d8f4cd82d3
developerdiyorbek <developersulaymonov1907@gmail.com>
2025-09-08 13:39:31 +0500

    some changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,24 @@ const ovo = Ovo({
 });
 
 export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "",
+  metadataBase: new URL("https://www.yusufbek.uz"),
+  title: "Yusufbek Xolmuminov",
+  description: "Yusufbek's portfolio",
+  authors: [{ name: "Yusufbek Xolmuminov", url: "https://www.yusufbek.uz" }],
+  icons: { icon: "/favicon.png" },
+  keywords:
+    "yusufbek, yusufbek.uz, yusufbek xolmuminov, developer yusufbek, yusufbek developer",
+  openGraph: {
+    title: "Yusufbek Xolmuminov",
+    description: "Yusufbek's portfolio",
+    type: "website",
+    url: "https://www.yusufbek.uz",
+    locale: "uz_UZ",
+    images: "/user-image.jpg",
+    countryName: "Uzbekistan",
+    siteName: "yusufbek.uz",
+    emails: "yusufbek.xolmuminov@gmail.com",
+  },
 };
 
 export default function RootLayout({ children }: PropsWithChildren) {
